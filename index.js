580c85710e11bb2826b149cfae625be33fc86e57
AloelnGames <mussylini@gmail.com>
2023-03-15 20:05:19 -0500

    removed semicolon

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const search_str_len = search_str.length;
 async function get_id_of(handle){
     const page = await (await fetch(`https://www.youtube.com/@${
         handle.substring(0, 1) === '@' ? handle.substring(1) : handle
-    }`)).text();;
+    }`)).text();
     const index = page.indexOf(`<meta itemprop="channelId"`);
     if(index ===-1) return null;
     const closin = page.substring(index, page.indexOf(`"`, index + search_str_len)+2);
